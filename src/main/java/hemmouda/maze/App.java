c95383d72325c6a1feee108ec1f5664044be7071
Aymane Hemmouda <aymane.hemmouda@alumni.fh-aachen.de>
2024-06-19 16:00:02 +0200

    Update App.java
    Okay now working. It was expecting big-endian / utf8. I guess it was already utf8 but not big-endian. Anyhow, now it uses the actual classes used by the server so that's solved!

--- src/main/java/hemmouda/maze/App.java
+++ src/main/java/hemmouda/maze/App.java
@@ -4,8 +4,12 @@ import de.fhac.mazenet.server.generated.ClientRole;
 import de.fhac.mazenet.server.generated.LoginMessageData;
 import de.fhac.mazenet.server.generated.MazeCom;
 import de.fhac.mazenet.server.generated.MazeComMessagetype;
+import de.fhac.mazenet.server.networking.UTFOutputStream;
+import de.fhac.mazenet.server.networking.XmlInputStream;
+import de.fhac.mazenet.server.networking.XmlOutputStream;
 import hemmouda.maze.communication.xml.Serializer;
 
+import javax.xml.stream.XMLOutputFactory;
 import java.io.*;
 import java.net.Socket;
 
@@ -14,30 +18,46 @@ public interface App {
     public static void main(String[] args) throws Exception {
         Socket socket = new Socket("localhost", 5123);
         System.out.println("Connected");
-//        var in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-//        var out = new PrintWriter(socket.getOutputStream());
-        DataInputStream dataInputStream = new DataInputStream(socket.getInputStream());
-        DataOutputStream dataOutputStream = new DataOutputStream(socket.getOutputStream());
+
+        var in = new XmlInputStream(socket.getInputStream());
+        var out = new XmlOutputStream(socket.getOutputStream());
+
         LoginMessageData loginMessageData = new LoginMessageData();
-        loginMessageData.setName("PP");
+        loginMessageData.setName("PP wiiii");
         loginMessageData.setRole(ClientRole.PLAYER);
         MazeCom com = new MazeCom();
         com.setLoginMessage(loginMessageData);
         com.setMessagetype(MazeComMessagetype.LOGIN);
         com.setId(-5);
-        Serializer serializer = new Serializer();
 
-        var msg = serializer.messageToXMLString(com);
-//        out.write(msg);
-//        out.flush();
-        dataOutputStream.writeUTF(msg);
-        dataOutputStream.flush();
+        out.write(com);
+        System.out.println("Sent");
 
-        System.out.println("Sent: " +msg);
-//        var line = in.readLine();
-        var line = dataInputStream.readUTF();
-        System.out.println("Read line");
-        System.out.println(line);
+        var response = in.readMazeCom();
+        System.out.println("Read: "+response);
+        System.out.println(response.getMessagetype());
+
+
+//        var in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+//        var out = new PrintWriter(socket.getOutputStream());
+//        UTFOutputStream utfOutputStream = new UTFOutputStream(socket.getOutputStream());
+//        DataInputStream dataInputStream = new DataInputStream(socket.getInputStream());
+//        DataOutputStream dataOutputStream = new DataOutputStream(socket.getOutputStream());
+//        Serializer serializer = new Serializer();
+//
+//        var msg = serializer.messageToXMLString(com);
+////        out.write(msg);
+////        out.flush();
+////        utfOutputStream.writeUTF8(msg);
+////        utfOutputStream.flush();
+//        dataOutputStream.writeUTF(msg);
+//        dataOutputStream.flush();
+//
+//        System.out.println("Sent: " +msg);
+////        var line = in.readLine();
+//        var line = dataInputStream.readUTF();
+//        System.out.println("Read line");
+//        System.out.println(line);
     }
 
 }

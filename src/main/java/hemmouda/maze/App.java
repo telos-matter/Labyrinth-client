83813f2268440480e861d7af60de4d7cf43c4649
Aymane Hemmouda <aymane.hemmouda@alumni.fh-aachen.de>
2024-06-20 23:29:55 +0200

    Update App.java
    Cleaning main

--- src/main/java/hemmouda/maze/App.java
+++ src/main/java/hemmouda/maze/App.java
@@ -1,63 +1,30 @@
 package hemmouda.maze;
 
-import de.fhac.mazenet.server.generated.ClientRole;
-import de.fhac.mazenet.server.generated.LoginMessageData;
-import de.fhac.mazenet.server.generated.MazeCom;
-import de.fhac.mazenet.server.generated.MazeComMessagetype;
-import de.fhac.mazenet.server.networking.UTFOutputStream;
-import de.fhac.mazenet.server.networking.XmlInputStream;
-import de.fhac.mazenet.server.networking.XmlOutputStream;
-import hemmouda.maze.communication.xml.Serializer;
+import hemmouda.maze.communication.Communicator;
+import hemmouda.maze.util.Logger;
 
-import javax.xml.stream.XMLOutputFactory;
-import java.io.*;
-import java.net.Socket;
-
+/**
+ * Let make it clear from the start:
+ * This app will be designed and developed
+ * with the requirement in mind that
+ * it will only ever run 1 single client at
+ * a time. So there will be a lot
+ * of singleton in here.
+ * And all the parts are aware
+ * and take that into their design
+ * that an instance of this App
+ * will only be running 1 client.
+ */
 public interface App {
 
-    public static void main(String[] args) throws Exception {
-        Socket socket = new Socket("localhost", 5123);
-        System.out.println("Connected");
- 
-        var in = new XmlInputStream(socket.getInputStream());
-        var out = new XmlOutputStream(socket.getOutputStream());
+    public static void main(String[] args) {
+        Logger.info("Starting the application");
 
-        LoginMessageData loginMessageData = new LoginMessageData();
-        loginMessageData.setName("PP wiiii");
-        loginMessageData.setRole(ClientRole.PLAYER);
-        MazeCom com = new MazeCom();
-        com.setLoginMessage(loginMessageData);
-        com.setMessagetype(MazeComMessagetype.LOGIN);
-        com.setId(-5);
+        Communicator communicator = Communicator.getCommunicator();
+        communicator.initialize();
+        communicator.beginGame();
 
-        out.write(com);
-        System.out.println("Sent");
-
-        var response = in.readMazeCom();
-        System.out.println("Read: "+response);
-        System.out.println(response.getMessagetype());
-
-
-//        var in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-//        var out = new PrintWriter(socket.getOutputStream());
-//        UTFOutputStream utfOutputStream = new UTFOutputStream(socket.getOutputStream());
-//        DataInputStream dataInputStream = new DataInputStream(socket.getInputStream());
-//        DataOutputStream dataOutputStream = new DataOutputStream(socket.getOutputStream());
-//        Serializer serializer = new Serializer();
-//
-//        var msg = serializer.messageToXMLString(com);
-////        out.write(msg);
-////        out.flush();
-////        utfOutputStream.writeUTF8(msg);
-////        utfOutputStream.flush();
-//        dataOutputStream.writeUTF(msg);
-//        dataOutputStream.flush();
-//
-//        System.out.println("Sent: " +msg);
-////        var line = in.readLine();
-//        var line = dataInputStream.readUTF();
-//        System.out.println("Read line");
-//        System.out.println(line);
+        Logger.info("Ending the application");
     }
 
 }

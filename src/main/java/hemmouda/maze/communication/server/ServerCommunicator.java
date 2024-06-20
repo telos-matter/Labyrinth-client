0f9c7051f8b5d387fd678a4f05072ee0abb8b96c
Aymane Hemmouda <aymane.hemmouda@alumni.fh-aachen.de>
2024-06-20 23:52:07 +0200

    Update ServerCommunicator.java
    Implemented initialize

--- src/main/java/hemmouda/maze/communication/server/ServerCommunicator.java
+++ src/main/java/hemmouda/maze/communication/server/ServerCommunicator.java
@@ -1,6 +1,13 @@
 package hemmouda.maze.communication.server;
 
+import de.fhac.mazenet.server.networking.XmlInputStream;
+import de.fhac.mazenet.server.networking.XmlOutputStream;
 import hemmouda.maze.communication.Communicator;
+import hemmouda.maze.settings.Settings;
+import hemmouda.maze.util.Logger;
+
+import java.io.IOException;
+import java.net.Socket;
 
 /**
  * Communicates with the actual server over TCP
@@ -16,12 +23,24 @@ public final class ServerCommunicator implements Communicator {
         return instance;
     }
 
+    private Socket socket;
+    private XmlInputStream in;
+    private XmlOutputStream out;
+
     private ServerCommunicator () {}
 
     @Override
     public void initialize() {
-        // TODO impl
-        throw new UnsupportedOperationException("Not yet implemented!");
+        try {
+            socket = new Socket(Settings.SERVER_ADDRESS, Settings.SERVER_PORT);
+            in = new XmlInputStream(socket.getInputStream());
+            out = new XmlOutputStream(socket.getOutputStream());
+        } catch (Exception e) {
+            Logger.error("An error occurred while trying to establish a connection with the server at %s:%d because of `%s`.", Settings.SERVER_ADDRESS.getHostAddress(), Settings.SERVER_PORT, e.getMessage());
+            throw new RuntimeException(e);
+        }
+
+        Logger.info("ServerCommunicator initialized successfully");
     }
 
     @Override

9f7f3adb408ebe58dcd99ca7075b6214fb1dd190
Aymane Hemmouda <aymane.hemmouda@alumni.fh-aachen.de>
2024-06-21 21:53:46 +0200

    Create UnexpectedResponse.java

---
+++ src/main/java/hemmouda/maze/util/exceptions/UnexpectedResponse.java
@@ -0,0 +1,15 @@
+package hemmouda.maze.util.exceptions;
+
+import de.fhac.mazenet.server.generated.MazeCom;
+import de.fhac.mazenet.server.generated.MazeComMessagetype;
+
+/**
+ * For responses you didn't expect
+ */
+public class UnexpectedResponse extends RuntimeException {
+
+    public UnexpectedResponse (MazeCom received, MazeComMessagetype expected) {
+        super("Received `%s`. But was expecting `%s`".formatted(received.getMessagetype(), expected));
+    }
+
+}

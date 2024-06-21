0a3f08c52bd93338f16077f3fc25f86a0791998e
Aymane Hemmouda <aymane.hemmouda@alumni.fh-aachen.de>
2024-06-21 23:27:11 +0200

    Create GameStatus.java

---
+++ src/main/java/hemmouda/maze/game/GameStatus.java
@@ -0,0 +1,9 @@
+package hemmouda.maze.game;
+
+public enum GameStatus {
+
+    PREPARING,      // Preparing to begin
+    IN_PROGRESS,    // Game currently running
+    COMPLETED       // Game finished
+
+}

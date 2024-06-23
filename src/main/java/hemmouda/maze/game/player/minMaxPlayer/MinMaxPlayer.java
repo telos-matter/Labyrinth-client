8fb016c3fa0bb292f24b9883ce2ef98544ade190
Aymane Hemmouda <aymane.hemmouda@alumni.fh-aachen.de>
2024-06-23 14:01:04 +0200

    Create MinMaxPlayer.java

---
+++ src/main/java/hemmouda/maze/game/player/minMaxPlayer/MinMaxPlayer.java
@@ -0,0 +1,33 @@
+package hemmouda.maze.game.player.minMaxPlayer;
+
+import de.fhac.mazenet.server.generated.AwaitMoveMessageData;
+import de.fhac.mazenet.server.generated.MoveMessageData;
+import hemmouda.maze.game.player.Player;
+
+/**
+ * A player that gets the best move with a MinMax algorithm
+ */
+public final class MinMaxPlayer implements Player {
+
+    private static MinMaxPlayer instance;
+
+    public static MinMaxPlayer getInstance() {
+        if (instance == null) {
+            instance = new MinMaxPlayer();
+        }
+        return instance;
+    }
+
+    private MinMaxPlayer () {}
+
+    @Override
+    public void initialize() {
+        throw new UnsupportedOperationException("Not yet implemented!");
+    }
+
+    @Override
+    public MoveMessageData getMove(AwaitMoveMessageData message) {
+        throw new UnsupportedOperationException("Not yet implemented!");
+    }
+
+}

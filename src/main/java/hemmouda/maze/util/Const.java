3e7d2254e67a0a0b62fb018f7a7819e628d073b6
Aymane Hemmouda <aymane.hemmouda@alumni.fh-aachen.de>
2024-06-23 16:12:06 +0200

    Create Const.java

---
+++ src/main/java/hemmouda/maze/util/Const.java
@@ -0,0 +1,13 @@
+package hemmouda.maze.util;
+
+import java.util.List;
+
+/**
+ * A constants record
+ */
+public final class Const {
+
+    public static final List <Integer> POSSIBLE_SHIFT_ROWS = List.of(1, 3, 5);
+    public static final List <Integer> POSSIBLE_SHIFT_COLS = List.of(1, 3, 5);
+
+}

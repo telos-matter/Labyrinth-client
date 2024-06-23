6b142a6c83996460110a0e7b7ccd39d7bf88d898
Aymane Hemmouda <aymane.hemmouda@alumni.fh-aachen.de>
2024-06-23 13:29:05 +0200

    Update App.java
    Docs + refactor of Game

--- src/main/java/hemmouda/maze/App.java
+++ src/main/java/hemmouda/maze/App.java
@@ -2,21 +2,22 @@ package hemmouda.maze;
 
 import de.fhac.mazenet.server.generated.ObjectFactory;
 import hemmouda.maze.communication.Communicator;
-import hemmouda.maze.game.Game;
+import hemmouda.maze.game.GameInfo;
 import hemmouda.maze.util.Logger;
 
 /**
- * Let make it clear from the start:
+ * Let's make it clear from the start:
  * This app will be designed and developed
  * with the requirement in mind that
  * it will only ever run 1 single player at
  * a time. So there will be a lot
- * of singleton in here.
+ * of singletons in here.
  * And all the parts are aware
  * and take into their design
  * that an instance of this App
  * will only be running 1 player.
- * One player. Not a manager.
+ * One player. Not a manager or a spectator.
+ * Also only one game.
  */
 public interface App {
 
@@ -28,7 +29,7 @@ public interface App {
     public static void main(String[] args) {
         Logger.info("Starting the application");
 
-        Game.initialize();
+        GameInfo.initialize();
 
         Communicator communicator = Communicator.getCommunicator();
         communicator.initialize();

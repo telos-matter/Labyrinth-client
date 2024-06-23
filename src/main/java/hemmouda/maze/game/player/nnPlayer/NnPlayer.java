73a52ef0382b3c48752822c02cc2429295d3e104
Aymane Hemmouda <aymane.hemmouda@alumni.fh-aachen.de>
2024-06-23 14:01:07 +0200

    Create NnPlayer.java

---
+++ src/main/java/hemmouda/maze/game/player/nnPlayer/NnPlayer.java
@@ -0,0 +1,33 @@
+package hemmouda.maze.game.player.nnPlayer;
+
+import de.fhac.mazenet.server.generated.AwaitMoveMessageData;
+import de.fhac.mazenet.server.generated.MoveMessageData;
+import hemmouda.maze.game.player.Player;
+
+/**
+ * A Neural Network player.
+ */
+public final class NnPlayer implements Player {
+
+    private static NnPlayer instance;
+
+    public static NnPlayer getInstance() {
+        if (instance == null) {
+            instance = new NnPlayer();
+        }
+        return instance;
+    }
+
+    private NnPlayer () {}
+
+    @Override
+    public void initialize() {
+        throw new UnsupportedOperationException("Not yet implemented!");
+    }
+
+    @Override
+    public MoveMessageData getMove(AwaitMoveMessageData message) {
+        throw new UnsupportedOperationException("Not yet implemented!");
+    }
+
+}

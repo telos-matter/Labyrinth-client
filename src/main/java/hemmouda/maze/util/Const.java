9c07a912a5ff8a5ae6af7696f796659c734af7db
Aymane Hemmouda <aymane.hemmouda@alumni.fh-aachen.de>
2024-06-24 19:58:46 +0200

    Update Const.java
    Yeah you can definetly insert at 3, 3

--- src/main/java/hemmouda/maze/util/Const.java
+++ src/main/java/hemmouda/maze/util/Const.java
@@ -1,5 +1,7 @@
 package hemmouda.maze.util;
 
+import de.fhac.mazenet.server.game.Position;
+
 import java.util.List;
 
 /**
@@ -7,7 +9,24 @@ import java.util.List;
  */
 public final class Const {
 
-    public static final List <Integer> POSSIBLE_SHIFT_ROWS = List.of(1, 3, 5);
-    public static final List <Integer> POSSIBLE_SHIFT_COLS = List.of(1, 3, 5);
+    public static final List<Position> POSSIBLE_SHIFT_POSITIONS = List.of(
+            // Going clock wise
+            // Top shifts
+            new Position(0, 1),
+            new Position(0, 3),
+            new Position(0, 5),
+            // Right shifts
+            new Position(1, 6),
+            new Position(3, 6),
+            new Position(5, 6),
+            // Bottom shifts
+            new Position(6, 5),
+            new Position(6, 3),
+            new Position(6, 1),
+            // Left shifts
+            new Position(5, 0),
+            new Position(3, 0),
+            new Position(1, 0)
+    );
 
 }

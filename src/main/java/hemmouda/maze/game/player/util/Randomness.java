fd4bec894d8090b6a2f4ffa631147511b3633188
Aymane Hemmouda <aymane.hemmouda@alumni.fh-aachen.de>
2024-07-17 14:37:50 +0200

    Create Randomness.java

---
+++ src/main/java/hemmouda/maze/game/player/util/Randomness.java
@@ -0,0 +1,28 @@
+package hemmouda.maze.game.player.util;
+
+import hemmouda.maze.settings.Settings;
+
+import java.util.List;
+import java.util.Random;
+
+/**
+ * A randomness utility class
+ */
+public class Randomness {
+
+    public static final Random RAND = (Settings.SEED == null)? new Random() : new Random(Settings.SEED);
+
+    /**
+     * Returns a random element
+     * from the list, or <code>null</code>
+     * if the list is empty
+     */
+    public static <T> T getRandomElement (List<T> list) {
+        if (list.isEmpty()) {
+            return null;
+        }
+
+        return list.get(RAND.nextInt(list.size()));
+    }
+
+}

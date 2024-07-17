30f7e7fc6c42b4cbccf3fa1af96876ae02b11322
Aymane Hemmouda <aymane.hemmouda@alumni.fh-aachen.de>
2024-07-17 14:40:00 +0200

    Update BestTurnPlayer.java
    implemented

--- src/main/java/hemmouda/maze/game/player/bestTurnPlayer/BestTurnPlayer.java
+++ src/main/java/hemmouda/maze/game/player/bestTurnPlayer/BestTurnPlayer.java
@@ -1,11 +1,16 @@
 package hemmouda.maze.game.player.bestTurnPlayer;
 
 import de.fhac.mazenet.server.game.Board;
+import de.fhac.mazenet.server.game.Card;
+import de.fhac.mazenet.server.game.Position;
 import de.fhac.mazenet.server.generated.MoveInfoData;
 import de.fhac.mazenet.server.generated.Treasure;
 import de.fhac.mazenet.server.generated.MoveMessageData;
 import de.fhac.mazenet.server.generated.TreasuresToGoData;
+import hemmouda.maze.game.GameInfo;
 import hemmouda.maze.game.player.Player;
+import hemmouda.maze.game.player.bestTurnPlayer.core.MoveEvaluator;
+import hemmouda.maze.game.player.util.BoardUtil;
 
 import java.util.List;
 
@@ -30,7 +35,34 @@ public final class BestTurnPlayer extends Player {
 
     @Override
     protected MoveMessageData think(Board board, Treasure currentTreasure, List<TreasuresToGoData> remainingTreasures) {
-        return null;
+        MoveEvaluator.MoveRecord bestMove = null;
+
+        // For every rotation of the shift card
+        for (Card card : BoardUtil.getAllRotations(new Card(board.getShiftCard()))) {
+            // And for every shift position
+            for (Position shiftPosition : BoardUtil.getAllShiftPositions(board)) {
+                // Get the new board, then get both the treasure
+                // position and the player position on it
+                Board boardAfter = BoardUtil.fakeInsert(board, shiftPosition, card);
+                Position treasurePosition = BoardUtil.getTreasurePosition(boardAfter, currentTreasure);
+                Position playerPosition = boardAfter.findPlayer(GameInfo.getPlayerId());
+                // And for every reachable position within that board
+                for (Position position : boardAfter.getAllReachablePositions(playerPosition)) {
+                    // Check if we can reach the treasure
+                    if (position.equals(treasurePosition)) {
+                        // If so, that is the move
+                        return constructMoveMessage(card, shiftPosition, position);
+                    }
+
+                    // Otherwise, let MoveEvaluator pick the best move
+                    var moveRecord = MoveEvaluator.constructMoveRecord(card, shiftPosition, position, treasurePosition);
+                    bestMove = MoveEvaluator.pick(bestMove, moveRecord);
+                }
+            }
+        }
+
+        // If the treasure cannot be reached, return the best move
+        return bestMove.constructMoveMessageData();
     }
 
     @Override

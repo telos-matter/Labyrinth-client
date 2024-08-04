80c95cebc444cc38dede8336113a0fdbd0bf2525
Aymane Hemmouda <aymane.hemmouda@alumni.fh-aachen.de>
2024-08-04 22:19:21 +0200

    Update LookAheadPlayer.java
    First attempt. Does not work

--- src/main/java/hemmouda/maze/game/logic/player/lookAheadPlayer/LookAheadPlayer.java
+++ src/main/java/hemmouda/maze/game/logic/player/lookAheadPlayer/LookAheadPlayer.java
@@ -17,6 +17,7 @@ import hemmouda.maze.settings.Settings;
 import hemmouda.maze.util.Const;
 import hemmouda.maze.util.Logger;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -33,12 +34,6 @@ public final class LookAheadPlayer extends Player {
         return instance;
     }
 
-    /**
-     * An alias for {@link Settings#TURNS_AHEAD}
-     * for easy access.
-     */
-    private int depth = -1;
-
     private LookAheadPlayer() {}
 
     @Override
@@ -47,9 +42,8 @@ public final class LookAheadPlayer extends Player {
             Logger.error("The value for TURNS_AHEAD must be greater than or equal to zero. Value provided: %d", Settings.TURNS_AHEAD);
             throw new IllegalArgumentException("TURNS_AHEAD must be greater than or equal to zero");
         }
-        depth = Settings.TURNS_AHEAD;
 
-        Logger.info("LookAheadPlayer has been initialized, and will look %d turn(s) into the future.", depth);
+        Logger.info("LookAheadPlayer has been initialized, and will look %d turn(s) into the future.", Settings.TURNS_AHEAD);
     }
 
     /**
@@ -95,10 +89,62 @@ public final class LookAheadPlayer extends Player {
      */
     @Override
     protected MoveMessageData think(Board board, Treasure currentTreasure, List<TreasuresToGoData> remainingTreasures) {
-        // Where to go if the treasure is unreachable
-        Position alternativeGoal = Randomness.getRandomElement(Const.POSSIBLE_SHIFT_POSITIONS);
-        // The best move to get us to that alternativeGoal
-        MoveEvaluator.MoveRecord bestMove = null;
+        System.out.println("###########");
+        var move = exploreMoves(board, currentTreasure, Settings.TURNS_AHEAD);
+        System.out.printf("playing this: %n");
+        System.out.printf("insert at: %s and move to: %s%n" ,move.getShiftPosition(), move.getNewPinPos());
+        return move;
+//        // Where to go if the treasure is unreachable
+//        Position alternativeGoal = Randomness.getRandomElement(Const.POSSIBLE_SHIFT_POSITIONS);
+//        // The best move to get us to that alternativeGoal
+//        MoveEvaluator.MoveRecord bestMove = null;
+//
+//        // 418 moves on avg
+//
+//        // For every rotation of the shift card
+//        for (Card card : new Card(board.getShiftCard()).getPossibleRotations()) {
+//            // And for every shift position
+//            for (Position shiftPosition : BoardUtil.getAllShiftPositions(board)) {
+//                // Get the new board, then get both the treasure
+//                // position and the player position on it
+//                Board boardAfter = BoardUtil.fakeInsert(board, shiftPosition, card);
+//                Position treasurePosition = BoardUtil.getTreasurePosition(boardAfter, currentTreasure);
+//                Position playerPosition = boardAfter.findPlayer(GameInfo.getPlayerId());
+//                // And for every reachable position within that board
+//                for (Position position : boardAfter.getAllReachablePositions(playerPosition)) {
+//                    // Check if we can reach the treasure
+//                    if (position.equals(treasurePosition)) {
+//                        // If so, that is the move
+//                        Logger.debug("%s found direct route to treasure!", this);
+//                        return MoveMessageUtil.construct(card, shiftPosition, position);
+//                    }
+//
+//                    // Otherwise, let MoveEvaluator pick the best move
+//                    var moveRecord = MoveEvaluator.constructMoveRecord(card, shiftPosition, position, alternativeGoal);
+//                    bestMove = MoveEvaluator.pick(bestMove, moveRecord);
+//                }
+//            }
+//        }
+//
+//        // If the treasure cannot be reached, return the best move
+//        Logger.debug("%s couldn't find direct route to treasure, trying to go to %s instead.", this, alternativeGoal);
+//        return bestMove.constructMoveMessageData();
+    }
+
+    /**
+     * Ahhhhhh
+     *
+     * So it wasn't working because it explores the moves vertically
+     * and not horizontally. So any depth above 1 wouldn't work properly
+     * wtf even depth 1 with test board has a prob. eh too much to check why,
+     * let's rewrite this
+     */
+    public MoveMessageData exploreMoves (Board board, Treasure goal, int depth) {
+        // Where the boards after each move is stored
+        List <Pair<MoveMessageData, Board>> moves = null;
+        moves = new ArrayList<>(512); // 418 on avg
+
+        int moveIndex = 0;
 
         // For every rotation of the shift card
         for (Card card : new Card(board.getShiftCard()).getPossibleRotations()) {
@@ -107,27 +153,68 @@ public final class LookAheadPlayer extends Player {
                 // Get the new board, then get both the treasure
                 // position and the player position on it
                 Board boardAfter = BoardUtil.fakeInsert(board, shiftPosition, card);
-                Position treasurePosition = BoardUtil.getTreasurePosition(boardAfter, currentTreasure);
+                Position treasurePosition = BoardUtil.getTreasurePosition(boardAfter, goal);
                 Position playerPosition = boardAfter.findPlayer(GameInfo.getPlayerId());
                 // And for every reachable position within that board
                 for (Position position : boardAfter.getAllReachablePositions(playerPosition)) {
+
+                    moveIndex++;
+
+                    var move = MoveMessageUtil.construct(card, shiftPosition, position);
+
                     // Check if we can reach the treasure
                     if (position.equals(treasurePosition)) {
                         // If so, that is the move
-                        Logger.debug("%s found direct route to treasure!", this);
-                        return MoveMessageUtil.construct(card, shiftPosition, position);
+                        System.out.println("found" + depth);
+                        System.out.println("Move #" +moveIndex);
+                        System.out.printf("insert at: %s and move to: %s%n" ,move.getShiftPosition(), move.getNewPinPos());
+                        return move;
                     }
 
                     // Otherwise, let MoveEvaluator pick the best move
-                    var moveRecord = MoveEvaluator.constructMoveRecord(card, shiftPosition, position, alternativeGoal);
-                    bestMove = MoveEvaluator.pick(bestMove, moveRecord);
+                    moves.add(new Pair<>(move, boardAfter));
+
                 }
             }
         }
 
-        // If the treasure cannot be reached, return the best move
-        Logger.debug("%s couldn't find direct route to treasure, trying to go to %s instead.", this, alternativeGoal);
-        return bestMove.constructMoveMessageData();
+        if (Settings.TURNS_AHEAD == 0) {
+            System.out.println("Not this");
+            return Randomness.getRandomElement(moves).first;
+        }
+
+        if (depth == 0) {
+            return null;
+        }
+
+        for (int i = 0; i < moves.size(); i++) {
+            var move = moves.get(i);
+            if (exploreMoves(move.second, goal, depth -1) != null) {
+                System.out.println("smth?" + depth);
+                System.out.println("Space: " +moves.size());
+                System.out.println("Move #" +(i+1));
+                System.out.printf("insert at: %s and move to: %s%n" ,move.first.getShiftPosition(), move.first.getNewPinPos());
+                System.out.println("treasure will be at: " + BoardUtil.getTreasurePosition(move.second, goal));
+                return move.first;
+            }
+        }
+
+        if (Settings.TURNS_AHEAD == depth) {
+            System.out.println("nothing?" + depth);
+            return Randomness.getRandomElement(moves).first;
+        } else {
+            return null;
+        }
+    }
+
+    public static class Pair <F, S> {
+        public F first;
+        public S second;
+
+        public Pair (F first, S second) {
+            this.first = first;
+            this.second = second;
+        }
     }
 
     @Override
@@ -135,6 +222,6 @@ public final class LookAheadPlayer extends Player {
 
     @Override
     public String toString() {
-        return "LookAheadPlayer #" +depth;
+        return "LookAheadPlayer (" +Settings.TURNS_AHEAD +")";
     }
 }
